8ae99c300bef5c24381bb0919f3d14973f13deb7
Tanerx <11832079-Tanerx@users.noreply.gitlab.com>
2026-04-17 00:09:20 +0200

    Fixed damage indicator cap when using PacketEvents

--- mythiclib-plugin/src/main/java/io/lumine/mythic/lib/comp/packetevents/DamageParticleCapImpl.java
+++ mythiclib-plugin/src/main/java/io/lumine/mythic/lib/comp/packetevents/DamageParticleCapImpl.java
@@ -5,6 +5,7 @@ import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.particle.type.ParticleTypes;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerParticle;
 import io.lumine.mythic.lib.comp.protocollib.DamageParticleCap;
 import org.bukkit.entity.Player;
@@ -19,6 +20,7 @@ public class DamageParticleCapImpl extends DamageParticleCap<PacketSendEvent, Wr
             public void onPacketSend(PacketSendEvent event) {
                 if (event.getPacketType() != PacketType.Play.Server.PARTICLE) return;
                 final var packet = new WrapperPlayServerParticle(event);
+                if (!packet.getParticle().getType().equals(ParticleTypes.DAMAGE_INDICATOR)) return;
                 final var player = (Player) event.getPlayer();
                 DamageParticleCapImpl.this.onPacketSend(player, event, packet, packet.getParticleCount());
             }
